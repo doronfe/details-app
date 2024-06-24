011cbbcc2926d3b94efa95f42bfaf18fb2f482c5
doronfe <feldmandoron@campus.technion.ac.il>
2024-06-24 21:22:19 +0300

    create an attempts mechansim to avoid race condition, delete details.py

--- details.py
+++
@@ -1,7 +0,0 @@
-from src.details.app import app
-
-HOST='0.0.0.0'
-PORT=8001
-DEBUG=True
-
-app.run(host=HOST, port=PORT, debug=DEBUG)
--- src/details/app.py
+++ src/details/app.py
@@ -1,10 +1,29 @@
 import os,sys
 from flask import Flask, render_template, request, redirect
-from psycopg2 import connect
+from psycopg2 import connect, OperationalError
 from src.details.libs import connectionForm
+from time import sleep
 
 app = Flask(__name__)
-conn = connect(host="postgres-database", dbname="users-details", user="postgres", password="postgres")
+
+max_attempts = 5
+attempts = 0
+
+while attempts < max_attempts:
+    try:
+        conn = connect(host="postgres-database", dbname="users-details", user="postgres", password="postgres")
+        print("Connected to the database successfully!")
+        break
+    except OperationalError as e:
+        attempts += 1
+        print(f"Attempt {attempts} failed with error: {e}")
+        print(f"Waiting for 5 seconds before retrying...")
+        sleep(5)
+
+if attempts == max_attempts:
+    print("Could not connect to the database after several attempts.")
+
+
 cur=conn.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS details (name VARCHAR(255), email VARCHAR(255));""")
 add_user= ("INSERT INTO details (name, email) VALUES (%s, %s)")
